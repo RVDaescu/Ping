462b35d75bd597cc60e534e0d1776fc46f73caea
daescuradu@yahoo.com <radu@radu.romania.luxoft.com>
2017-07-27 17:01:39 +0300

    added some new lines

--- ping.py
+++ ping.py
@@ -1,8 +1,11 @@
 from re import *
 import pexpect
+import time
 
-def ping(host, count = 10):
+def ping(host, count = 50):
+    start = time.time() 
     child = pexpect.spawn('ping -i 1 -c %d %s' %(count, host))
+    child.timeout = 300
     output = []
     while 1:
         line = child.readline()
@@ -18,4 +21,6 @@ def ping(host, count = 10):
             pl = j.split(' ')
             pl = pl[1]
 
-    print '%s: %s packet loss\n' %(host, pl),
+    end = time.time() - start 
+
+    print '[%.2f s] - %s: %s packet loss\n' %(end, host, pl),
