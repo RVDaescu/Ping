2252bb5891160316ae3baac659702b1172d36b2e
Radu Daescu <daescuradu@yahoo.com>
2017-08-05 00:51:04 +0300

    changed password for email

--- mail.py
+++ mail.py
@@ -1,10 +1,13 @@
 import smtplib
+import base64
 
-def send_mail(to_addr = 'rvdaescu@gmail.com', msg = None)
+def send_mail(to_addr = 'rvdaescu@gmail.com', msg = None):
 
-    server = smtplib.SMTP('mail.vhsmail.vodafone.com', 25)
+    server = smtplib.SMTP_SSL('mail.vhsmail.vodafone.com', 465)
 
-    server.login('office@daescu.ro', 'Sugipula1@')
+    server.login('office@daescu.ro', base64.decodestring('U3VnaXB1bGExQA==\n'))
+
+    msg =  'Subject: TEST \n\n Mail from Office'
 
     msg = msg
 
