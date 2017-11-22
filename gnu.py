db7e0699a9b7a12763349fe83739a776672aa931
Radu <daescuradu@yahoo.com>
2017-11-23 01:23:39 +0200

    Some Changes

--- gnu.py
+++ gnu.py
@@ -4,30 +4,32 @@ import Gnuplot
 host = 'host.sql'
 tb = 'tb_80_86_107_69'
 
-db = sql()
-data = db.get_data(db_name = host,tb_name= tb, field = 'Reachability, Time')
+def plot_to_file(db_name, tb_name, fields = '*')
 
-g = Gnuplot.Gnuplot()
-g.title("Host 80.86.107.69")
+    db = sql()
+    data = db.get_data(db_name = db_name,tb_name= tb_name, field = fields)
 
-g.xlabel("Time")
-g.ylabel("Reachability (%)")
+    g = Gnuplot.Gnuplot()
+    g.title("Host %s",%(tb.replace('_','.')[3:]))
 
-time_list = [i[1] for i in data]
-reach_list = [i[0] for i in data]
+    g.xlabel("Time")
+    g.ylabel("Reachability (%)")
 
-xtic = int(max(time_list)-min(time_list))/25
+    time_list = [i[1] for i in data]
+    reach_list = [i[0] for i in data]
 
-g("set grid")
-g("set xtic %s" %xtic)
-g("set ytic 10")
+    xtic = int(max(time_list)-min(time_list))/25
+
+    g("set grid")
+    g("set xtic %s" %xtic)
+    g("set ytic 10")
 
 
-d = Gnuplot.Data (time_list, reach_list, title="Reachability", with_="lines")
+    d = Gnuplot.Data (time_list, reach_list, title="Reachability", with_="lines")
 
-g("set terminal svg")
-g.plot(d) # write SVG data directly to stdout ...
+    g("set terminal svg")
+    g.plot(d) # write SVG data directly to stdout ...
 
-g.hardcopy (filename='/home/radu/Ping/graphs/86.png', terminal='png') # write last plot to another terminal
+    g.hardcopy (filename='/home/radu/Ping/graphs/86.png', terminal='png') # write last plot to another terminal
 
-del g
+    del g
