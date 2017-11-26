f070033f1b3def3007321cf686818a8e83020393
Radu <daescuradu@yahoo.com>
2017-11-26 12:30:46 +0200

    New tests

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ ips = open('ip2.txt', 'r').readlines()
 ip = [i.strip('\n') for i in ips]
 
 for i in ip:
-    a = host(ip = i, db_name = 'host_gns.sqlite', pkt_count = 3, pkt_inter = 1, inter = 5, repeat_nr = 5, debug = False)
+    a = host(ip = i, db_name = 'host_gns.sqlite', pkt_count = 5, pkt_inter = 1, inter = 30, repeat_nr = 1000, debug = False)
     a.start()
