abd27dceb4e6a5b088e9e5a33dc625e8582a1014
RVDaescu <daescuradu@yahoo.com>
2018-05-08 11:23:13 +0300

    Tested run class for effectivness

--- run_tst.py
+++ run_tst.py
@@ -15,15 +15,15 @@ class main():
     tb = 'group_01'
     res_db = 'res_test.sqlite'
 
-    list = sql().get_data(db = db, tb = tb, key = 'ip')
+    list_from_db = sql().get_data(db = db, tb = tb, key = 'ip')
 
-    hd = list.pop(0)
+    hd = list_from_db.pop(0)
 
     to_run = []        #list of running hosts
     to_stop = []        #list of stopped hosts
     run_threads = []    #list of threads started
 
-    for i in list:
+    for i in list_from_db:
         if i[hd['Monitoring']] == 'True':
             a = monitor(host = i[hd['IP']],
                         pkt_count = i[hd['pkt_count']],
@@ -42,19 +42,27 @@ class main():
 
     sleep(30)
     while True:
-        list = sql().get_data(db = db, tb = tb, key = 'ip')
-        hd = list.pop(0)
+        list_from_db = sql().get_data(db = db, tb = tb, key = 'ip')
+        hd = list_from_db.pop(0)
         
-        for host in list:
+        for host in list_from_db:
             if host[hd['Monitoring']] == 'True' and host in to_stop:
                 to_run.append(to_stop.pop(host))
+                print 'moving %s from stop to run' %list(host)
             elif host[hd['Monitoring']] == 'False' and host in to_run:
                 to_stop.append(to_run.pop(host))
+                print 'moving %s from run to stop' %list(host)
             elif host[hd['Monitoring']] == 'True' and host not in to_run:
-                to_run.append(host)           
+                to_run.append(host)
+                print 'adding %s to run' %list(host)
             elif host[hd['Monitoring']] == 'False' and host not in to_stop:
                 to_stop.append(host)
-       
+                print 'adding %s to stop' %list(host)
+      
+        print 'threads: %s' %run_threads
+        print 'run: %s' %to_run
+        print 'stop: %s' %to_stop
+
         for thread in run_threads:
             for stp in to_stop:
                 if thread.name in stp:
@@ -62,9 +70,11 @@ class main():
                     run_threads.pop(run_threads.index(thread)) 
         
         run_threads_name = [t.name for t in run_threads]
+        
+        print 'run threads name: %s' %run_threads_name
 
         for st in to_run:
-            if st not in run_threads_name:
+            if st[hd['Name']] not in run_threads_name:
 		a = monitor(host = st[hd['IP']],
 			    pkt_count = st[hd['pkt_count']],
 			    pkt_inter = st[hd['pkt_inter']],
