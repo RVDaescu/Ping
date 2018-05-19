1ad0881b11bd92ff878f15fdb4b38cddedea6565
Radu <daescuradu@yahoo.com>
2018-05-20 01:11:25 +0300

    Improved run method to check forchanges of hosts and status

--- run.py
+++ run.py
@@ -11,19 +11,18 @@ sys.dont_write_bytecode = True
 
 class main():
     
-    db = database       
-    tb = table      
+    db = database
+    tb = table
     res_db = result_db
 
-    list = sql().get_data(db = db, tb = tb, key = 'ip')
+    list_from_db = sql().get_data(db = db, tb = tb, key = 'ip')
+    hd = list_from_db.pop(0)
 
-    hd = list.pop(0)
-
-    to_run = []        #list of running hosts
+    to_run = []         #list of running hosts
     to_stop = []        #list of stopped hosts
     run_threads = []    #list of threads started
 
-    for i in list:
+    for i in list_from_db:
         if i[hd['Monitoring']] == 'True':
             a = monitor(host = i[hd['IP']],
                         pkt_count = i[hd['pkt_count']],
@@ -31,50 +30,52 @@ class main():
                         inter = i[hd['interval']],
                         name = i[hd['Name']],
                         db = res_db)
-            to_run.append(i)
+            to_run.append(i[hd['Name']])
             a.start()
             run_threads.append(a)
             sleep(0.05)
             if len(to_run) % 10 == 0:
                 sleep(1)
         elif i[hd['Monitoring']] == 'False':
-            to_stop.append(i)
+            to_stop.append(i[hd['Name']])
 
     sleep(30)
     while True:
-        list = sql().get_data(db = db, tb = tb, key = 'ip')
-        hd = list.pop(0)
+        list_from_db = sql().get_data(db = db, tb = tb, key = 'ip')
+        hd = list_from_db.pop(0)
         
-        for host in list:
-            if host[hd['Monitoring'] == 'True' and host in to_stop:
-                to_run.append(to_stop.pop(host))
-            elif host[hd['Monitoring'] == 'False' and host in to_run:
-                to_stop.append(to_run.pop(host))
-            elif host[hd['Monitoring'] == 'True' and host not in to_run:
-                to_run.append(host)           
-            elif host[hd['Monitoring'] == 'False' and host not in to_stop:
-                to_stop.append(host)
-       
+        for host in list_from_db:
+            if host[hd['Monitoring']] == 'True' and host[hd['Name']] in to_stop:
+                to_run.append(to_stop.pop(to_stop.index(host[hd['Name']])))
+            elif host[hd['Monitoring']] == 'False' and host[hd['Name']] in to_run:
+                to_stop.append(to_run.pop(to_run.index(host[hd['Name']])))
+            elif host[hd['Monitoring']] == 'True' and host[hd['Name']] not in to_run:
+                to_run.append(host[hd['Name']]) 
+            elif host[hd['Monitoring']] == 'False' and host[hd['Name']] not in to_stop:
+                to_stop.append(host[hd['Name']])
+      
         for thread in run_threads:
             for stp in to_stop:
-                if thread.name in stp:
+                if thread.name == stp:
                     thread.work = False
+                    sleep(1)
                     run_threads.pop(run_threads.index(thread)) 
         
-        run_threads_name = [t.name for i in run_threads]
-
-        for st in to_run:
-            if st not in run_threads_name:
-		a = monitor(host = st[hd['IP']],
-			    pkt_count = st[hd['pkt_count']],
-			    pkt_inter = st[hd['pkt_inter']],
-			    inter = st[hd['interval']],
-			    name = st[hd['Name']],
-			    db = res_db)
-		a.start()
-		run_threads.append(a)
-		sleep(0.1)
-	sleep(60)
+        run_threads_name = [t.name for t in run_threads]
+        
+        for strt in to_run:
+            if strt not in run_threads_name:
+                for host in list_from_db:
+                    if host[hd['Name']] == strt:
+                        a = monitor(host = host[hd['IP']],
+                                    pkt_count = host[hd['pkt_count']],
+                                    pkt_inter = host[hd['pkt_inter']],
+                                    inter = host[hd['interval']],
+                                    name = host[hd['Name']],
+                                    db = res_db)
+                        a.start()
+                        run_threads.append(a)
+	sleep(30)
 
 if __name__ == '__main__':
     main()
