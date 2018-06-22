fcc285bd9323adf1da73630c107fb59e96fc5397
Radu V <daescuradu@yahoo.com>
2018-06-23 02:29:42 +0300

    Changed _config.py sql dbs

--- _config.py
+++ _config.py
@@ -9,5 +9,9 @@ lib_path = cwd + '\lib\\'
 #compatible for multiple sql databases
 #structure: {db:{table: "tb.db", results: "result.db"}
 dbs = {'hosts.sqlite': [{'table': 'Internet', 'results': 'res_db.sqlite'}, 
-                        {'table': 'Interlan', 'results': 'res_interlan.db'}]
+                        {'table': 'Interlan', 'results': 'res_interlan.db'}],
+       'proxy.sqlite': [{'table': 'Germany_elite', 'results': 'res_de.sqlite'}, 
+                        {'table': 'Franch_elite', 'results': 'res_fr.db'},
+                        {'table': 'Japan_elite', 'results': 'res_jp.sqlite'}, 
+                        {'table': 'Russia_elite', 'results': 'res_rs.db'}]
       }
